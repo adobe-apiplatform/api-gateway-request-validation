8a6c7cd084444511545e8e4bc299f86e7f8a0c26
Dragos Dascalita Haut <ddragosd@gmail.com>
2015-08-13 17:22:40 -0700

    # added license info

--- src/lua/api-gateway/validation/oauth2/userProfileValidator.lua
+++ src/lua/api-gateway/validation/oauth2/userProfileValidator.lua
@@ -1,3 +1,24 @@
+-- Copyright (c) 2015 Adobe Systems Incorporated. All rights reserved.
+--
+--   Permission is hereby granted, free of charge, to any person obtaining a
+--   copy of this software and associated documentation files (the "Software"),
+--   to deal in the Software without restriction, including without limitation
+--   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--   and/or sell copies of the Software, and to permit persons to whom the
+--   Software is furnished to do so, subject to the following conditions:
+--
+--   The above copyright notice and this permission notice shall be included in
+--   all copies or substantial portions of the Software.
+--
+--   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--   DEALINGS IN THE SOFTWARE.
+
+
 -- User Profile validator.
 -- Use this validator to add profile properties in request variables that may be passed forward to the backend service.
 -- This speeds up the process to identify who the user is so that the backend service doesn't have to worry about it.
