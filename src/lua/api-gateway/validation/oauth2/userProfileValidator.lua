61f25e1be10f46439043b0367347ddfe95b0ab81
Trifan Alexandru <atrifan@users.noreply.github.com>
2017-06-15 14:30:50 +0300

    fixing missfire of userProfile and mathemtical error of expiration (#24)

--- src/lua/api-gateway/validation/oauth2/userProfileValidator.lua
+++ src/lua/api-gateway/validation/oauth2/userProfileValidator.lua
@@ -58,6 +58,10 @@ local RESPONSES = {
 -- Maximum time in seconds specifying how long to cache a valid token in GW's memory
 local LOCAL_CACHE_TTL = 60
 
+---
+-- Maximum time in milliseconds specifying how long to cache a valid token in Redis
+local REDIS_CACHE_TTL = 6 * 60 * 60
+
 -- returns the key that should be used when looking up in the cache --
 function _M:getCacheToken(token)
     local t = token;
@@ -125,12 +129,22 @@ function _M:storeProfileInCache(cacheLookupKey, cachingObj)
 
     local oauthTokenExpiration = ngx.ctx.oauth_token_expires_at
     local expiresIn = self:getExpiresIn(oauthTokenExpiration)
+
+    if ( expiresIn <= 0 ) then
+        ngx.log(ngx.DEBUG, "OAuth Token was not persisted in the cache as it has expired at:" .. tostring(expiresIn) .. ", while now is:" .. tostring(ngx.time() * 1000) .. " ms.")
+        return nil
+    end
+
     local localExpiresIn = math.min( expiresIn, LOCAL_CACHE_TTL )
     ngx.log(ngx.DEBUG, "Storing new cached User Profile in the local cache for " .. tostring(localExpiresIn) .. " s out of a total validity of " .. tostring(expiresIn) .. " s.")
 
+    local default_ttl_expire = REDIS_CACHE_TTL
+    if ngx.var.max_oauth_redis_cache_ttl ~= nil and ngx.var.max_oauth_redis_cache_ttl ~= '' then
+        default_ttl_expire = ngx.var.max_oauth_redis_cache_ttl
+    end
     self:setKeyInLocalCache(cacheLookupKey, cachingObjString, localExpiresIn , "cachedUserProfiles")
     -- cache the use profile for 5 minutes
-    self:setKeyInRedis(cacheLookupKey, "user_json", oauthTokenExpiration or ((ngx.time() + LOCAL_CACHE_TTL) * 1000 ), cachingObjString)
+    self:setKeyInRedis(cacheLookupKey, "user_json", math.min(expiresIn, (ngx.time() + default_ttl_expire) * 1000), cachingObjString)
 end
 
 --- Returns true if the profile is valid for the request context
