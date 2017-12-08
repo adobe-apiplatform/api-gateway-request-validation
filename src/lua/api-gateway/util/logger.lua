fe81b59553b701ed96e9d221d4230f3822631bce
trifan <trifan@adobe.com>
2017-12-08 12:18:09 +0200

    update logFormat

--- src/lua/api-gateway/util/logger.lua
+++ src/lua/api-gateway/util/logger.lua
@@ -1,15 +1,21 @@
 local set = false
 
+function getLogFormat(level, debugInfo, ...)
+    return level, "[", debugInfo.short_src,
+        ":", debugInfo.currentline,
+        ":", debugInfo.name,
+        " req_id=", ngx.var.requestId,
+        "] ", ...
+end
+
 function _decorateLogger()
     if not set then
         local oldNgx = ngx.log
         ngx.log = function(level, ...)
             local debugInfo =  debug.getinfo(2)
+            local args = ...
             pcall(function()
-                oldNgx(level, "[", debugInfo.short_src,
-                ":", debugInfo.currentline,
-                ":", debugInfo.name,
-                " req_id=", ngx.var.requestId, "] ", ...)
+                oldNgx(getLogFormat(level, debugInfo, args))
             end)
         end
         set = true
