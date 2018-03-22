b2dec5eadf266124fe380ff2f6d3d49a9b41e5c4
Andra Lungu <lungu@adobe.com>
2018-03-22 15:17:57 +0200

    Debugging

--- dogstatsd/Dogstatsd.lua
+++ dogstatsd/Dogstatsd.lua
@@ -8,60 +8,6 @@ function Dogstatsd:new(o)
     return o
 end
 
---- Loads a lua gracefully. If the module doesn't exist the exception is caught, logged and the execution continues
--- @param module path to the module to be loaded
---
-local function loadrequire(module)
-    ngx.log(ngx.DEBUG, "Loading module [" .. tostring(module) .. "]")
-    local function requiref(module)
-        require(module)
-    end
-
-    local res, cls = pcall(requiref, module)
-    if res then
-        return cls
-    else
-        ngx.log(ngx.WARN, "Could not load module [", module, "].")
-        return nil
-    end
-end
-
---- Returns an instance of dogstatsd only if it does not already exist
-function Dogstatsd:getDogstatsd()
-
-    if dogstatsd ~= nil then
-        return dogstatsd
-    end
-
-    local restyDogstatsd = loadrequire('resty_dogstatsd')
-
-    if restyDogstatsd == nil then
-        return nil
-    end
-
-    local dogstatsd = restyDogstatsd.new({
-        statsd = {
-            host = "datadog.docker",
-            port = 8125,
-            namespace = "api_gateway",
-        },
-        tags = {
-            "application:lua",
-        },
-    })
-    return dogstatsd
-end
-
---- Increments the number of calls to the Oauth provider
-function Dogstatsd:incrementOauthCalls(oauthCalls)
-    local dogstatsd
-    if self.dogstatsd == nil then
-        dogstatsd = self:getDogstatsd()
-    end
-    if dogstatsd ~= nil then
-        dogstatsd:increment(oauthCalls, 1)
-    end
-end
 
 return Dogstatsd
 
