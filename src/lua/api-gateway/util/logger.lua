1b29b3507e0a96eead9faa950472439b5e975276
trifan <trifan@adobe.com>
2018-04-02 11:45:28 +0300

    fixing logger

--- src/lua/api-gateway/util/logger.lua
+++ src/lua/api-gateway/util/logger.lua
@@ -1,14 +1,27 @@
 local set = false
 
-function getLogFormat(level, debugInfo, ...)
-    return level, "[", debugInfo.short_src,
-        ":", debugInfo.currentline,
-        ":", debugInfo.name,
-        "() req_id=", tostring(ngx.var.requestId),
-        "] ", ...
+local function is_in_init_phase()
+    return ngx.var.requestId
 end
 
-function _decorateLogger()
+local function getLogFormat(level, debugInfo, ...)
+    local status, result = pcall(is_in_init_phase)
+    if status then
+        return level, "[", debugInfo.short_src,
+        ":", debugInfo.currentline,
+        ":", debugInfo.name,
+        "() req_id=", tostring(result),
+        "] ", ...
+    else
+        return 'NOTICE', "[", debugInfo.short_src,
+        ":", debugInfo.currentline,
+        ":", debugInfo.name,
+        "() req_id=",
+        "] ", ...
+    end
+end
+
+local function _decorateLogger()
     if not set then
         local oldNgx = ngx.log
         ngx.log = function(level, ...)
