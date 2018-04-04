67540f760637a5e613f70486a2863b0c569e0850
trifan <trifan@adobe.com>
2018-04-04 10:22:25 +0300

    update docs

--- src/lua/api-gateway/util/logger.lua
+++ src/lua/api-gateway/util/logger.lua
@@ -1,9 +1,20 @@
 local set = false
 
+---
+-- Checks and returns the ngx.var.requestId if possible
+-- ngx.var is not accessible in some nginx phases like init phase and so we also check this
+--
 local function is_in_init_phase()
     return ngx.var.requestId
 end
 
+---
+-- Get an error log format level, [file:currentline:function_name() req_id=<request_id>], message. This is passed to the
+-- original ngx.log function
+-- @param level - the log level like ngx.DEBUG, ngx.INFO, etc.
+-- @param debugInfo - the debug.getinfo() table needed for the stacktrace
+-- @param ... - other variables normally passed to ngx.log(), in general string concatenation
+--
 local function getLogFormat(level, debugInfo, ...)
     local status, request_id = pcall(is_in_init_phase)
     --- testing for init phase
@@ -18,6 +29,9 @@ local function getLogFormat(level, debugInfo, ...)
     "] ", ...
 end
 
+---
+-- Replaces the ngx.log function with the original ngx.log but redecorate the message
+--
 local function _decorateLogger()
     if not set then
         local oldNgx = ngx.log
