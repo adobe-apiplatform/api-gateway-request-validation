03b5681d47e5af0c2d92e165bba0c50a35ec791c
Alexandru Trifan <trifan.alex.criss@gmail.com>
2018-04-04 10:31:26 +0300

    [WS-11856] Fix logger decorator for callbacks (#60)
    * fixing logger
    
    * fixing the logger
    
    * better getinfo
    
    * update format for logger
    
    * fixing logger
    
    * fixing the logger
    
    * better getinfo
    
    * update format for logger
    
    * update logger
    
    * update docs
    
    * additional docs
    
    * update docs

--- src/lua/api-gateway/util/logger.lua
+++ src/lua/api-gateway/util/logger.lua
@@ -1,18 +1,44 @@
 local set = false
 
-function getLogFormat(level, debugInfo, ...)
-    return level, "[", debugInfo.short_src,
-        ":", debugInfo.currentline,
-        ":", debugInfo.name,
-        "() req_id=", tostring(ngx.var.requestId),
-        "] ", ...
+---
+-- Checks and returns the ngx.var.requestId if possible
+-- ngx.var is not accessible in some nginx phases like init phase and so we also check this
+--
+local function is_in_init_phase()
+    return ngx.var.requestId
 end
 
-function _decorateLogger()
+---
+-- Get an error log format level, [file:currentline:function_name() req_id=<request_id>], message. This is passed to the
+-- original ngx.log function
+-- @param level - the log level like ngx.DEBUG, ngx.INFO, etc.
+-- @param debugInfo - the debug.getinfo() table needed for the stacktrace
+-- @param ... - other variables normally passed to ngx.log(), in general string concatenation
+--
+local function getLogFormat(level, debugInfo, ...)
+    local status, request_id = pcall(is_in_init_phase)
+    --- testing for init phase
+    if not status then
+       request_id = "N/A"
+    end
+
+    return level, "[", debugInfo.short_src,
+    ":", debugInfo.currentline,
+    ":", debugInfo.name,
+    "() req_id=", tostring(request_id),
+    "] ", ...
+end
+
+---
+-- Replaces the ngx.log function with the original ngx.log but redecorate the message
+--
+local function _decorateLogger()
     if not set then
         local oldNgx = ngx.log
         ngx.log = function(level, ...)
-            local debugInfo =  debug.getinfo(2)
+            -- gets the level 2 because level 1 is this function and I need my caller
+            -- nSl means line, name, source
+            local debugInfo =  debug.getinfo(2, "nSl")
             pcall(function(...)
                 oldNgx(getLogFormat(level, debugInfo, ...))
             end, ...)
