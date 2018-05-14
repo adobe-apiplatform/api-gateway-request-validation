83de90615f37859466cd092c6dc5c4f0501e105a
Andra Lungu <lungu@adobe.com>
2018-05-14 17:18:55 +0300

    Fixed test for a valid json.

--- test/unit-tests/api-gateway/validation/oauth2/oauthTokenValidatorTest.lua
+++ test/unit-tests/api-gateway/validation/oauth2/oauthTokenValidatorTest.lua
@@ -18,35 +18,49 @@
 --   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 --   DEALINGS IN THE SOFTWARE.
 
-local cjson = require "cjson"
 local safeCjson = require "cjson.safe"
-
-local BaseValidatorMock = mock("api-gateway.validation.validator", {
-    "new", "exitFn", "getKeyFromLocalCache", "setContextProperties",
-    "getKeyFromRedis", "setKeyInLocalCache"
-})
-
+local redisMock = mock("resty.redis", {"new"})
+local shared = mock("ngx.shared", {"safe_set", "delete"})
 local RedisConnectionProviderMock = mock("api-gateway.redis.redisConnectionProvider", {
     "new", "getConnection", "closeConnection"
 })
 
-local EXPECTED_RESPONSES = {
-    MISSING_TOKEN = { error_code = "403010", message = "Oauth token is missing" },
-    INVALID_TOKEN = { error_code = "401013", message = "Oauth token is not valid" },
-    TOKEN_MISSMATCH = { error_code = "401014", message = "Token not allowed in the current context" },
-    SCOPE_MISMATCH = { error_code = "401015", message = "Scope mismatch" },
-    UNKNOWN_ERROR = { error_code = "503010", message = "Could not validate the oauth token" }
-}
+beforeEach(function()
+    ngx.config = {
+        debug = false
+    }
+
+    ngx.__time.doReturn = function()
+        return 123
+    end
+
+    ngx.shared = {
+        cachedOauthTokens = shared
+    }
+end)
 
 test('checkResponseFromAuth: should return false for an invalid json', function()
     local classUnderTest = require('api-gateway.validation.oauth2.oauthTokenValidator'):new()
-    local tokenValidity, error = classUnderTest.checkResponseFromAuth("invalid_json", "key")
+    local tokenValidity, error = classUnderTest:checkResponseFromAuth("invalid_json", "key")
     assertEquals(tokenValidity, false)
 end)
 
 test('checkResponseFromAuth: should return true for a valid json', function()
     local classUnderTest = require('api-gateway.validation.oauth2.oauthTokenValidator'):new()
-    local tokenValidity, error = classUnderTest.checkResponseFromAuth("{\"valid\":true,\"expires_at\":$generated_expires_at,\"token\": {\"id\":\"1234\",\"scope\":\"openid email profile\",\"user_id\":\"21961FF44F97F8A10A490D36\",\"expires_in\":\"86400000\",\"client_id\":\"client_id_test_2\",\"type\":\"access_token\"}}", "key")
-    assertEquals(tokenValidity, false)
+    local token = {
+        id = "1234", scope = "openid email profile", user_id = "21961FF44F97F8A10A490D36", expires_in = "86400000", client_id = "client_id_test_2", type = "access_token"
+    }
+
+    local body = {
+        valid = true, expires_at = 3400, token = safeCjson.encode(token)
+    }
+
+    local testJson = {
+        status = 200,
+        body = safeCjson.encode(body)
+    }
+
+    local tokenValidity = classUnderTest:checkResponseFromAuth(testJson, "key")
+    assertEquals(tokenValidity, true)
 end)
 
