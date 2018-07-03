32ded42129158c11c926112c3f0e4e4e1517e945
trifan <trifan@adobe.com>
2018-07-03 15:22:33 +0300

    add additional tests + luacov mapping

--- run_tests.lua
+++ run_tests.lua
@@ -6,7 +6,8 @@
 local tests = {
     "test.unit-tests.api-gateway.validation.redisApiKeyValidatorTest",
     "test.unit-tests.api-gateway.util.hasherTest",
-    "test.unit-tests.api-gateway.validation.oauth2.oauthTokenValidatorTest"
+    "test.unit-tests.api-gateway.validation.oauth2.oauthTokenValidatorTest",
+    "test.unit-tests.api-gateway.validation.oauth2.userProfileValidatorTest"
 }
 
 require("mocka.suite")(tests)
